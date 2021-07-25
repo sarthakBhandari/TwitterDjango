52aabd146c7cced5c1d0088264de0b92136a48b1
sarthakBhandari <sarthak9899bh@gmail.com>
2021-07-25 16:13:04 +0100

    github push

--- tweetme2/settings/production.py
+++ tweetme2/settings/production.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-j@nk34gdu(yf8beiw&&iyqrxmi=a!_yd(7kpwigs#7f(zh$87o')
+SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
