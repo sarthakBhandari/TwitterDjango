e69912e2b32059f335231791d6a19645209166ee
sarthakBhandari <sarthak9899bh@gmail.com>
2021-07-25 15:09:28 +0100

    fixing url in react

--- tweetme2-web/src/lookup/components.js
+++ tweetme2-web/src/lookup/components.js
@@ -20,7 +20,7 @@ export function backendLookup(method, endpoint, callback, data) {
     jsonData = JSON.stringify(data);
   }
   const xhr = new XMLHttpRequest();
-  const url = `http://localhost:8000/api${endpoint}`;
+  const url = `http://tweetme2-deploy.herokuapp.com/api${endpoint}`;
   xhr.responseType = "json";
   const csrftoken = getCookie("csrftoken");
   xhr.open(method, url);
