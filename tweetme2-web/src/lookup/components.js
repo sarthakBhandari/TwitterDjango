06f30138ba36bb8252e5f669178fe492b9bc4992
sarthakBhandari <sarthak9899bh@gmail.com>
2021-07-25 15:38:22 +0100

    https from http

--- tweetme2-web/src/lookup/components.js
+++ tweetme2-web/src/lookup/components.js
@@ -20,7 +20,7 @@ export function backendLookup(method, endpoint, callback, data) {
     jsonData = JSON.stringify(data);
   }
   const xhr = new XMLHttpRequest();
-  const url = `http://tweetme2-deploy.herokuapp.com/api${endpoint}`;
+  const url = `https://tweetme2-deploy.herokuapp.com/api${endpoint}`;
   xhr.responseType = "json";
   const csrftoken = getCookie("csrftoken");
   xhr.open(method, url);
